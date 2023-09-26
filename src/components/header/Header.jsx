d730474964b4ee68959a7827c1bc81832642e576
Jayesh <123942397+jayeshvagh11@users.noreply.github.com>
2023-09-26 11:36:22 +0530

    Final-commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./style.scss";
 
@@ -17,6 +18,7 @@ const Header = () => {
     const [showSearch, setShowSearch] = useState("");
     const navigate = useNavigate();
     const location = useLocation();
+    const { loginWithRedirect,isAuthenticated,logout, user } = useAuth0();
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -71,56 +73,62 @@ const Header = () => {
     };
 
     return (
-        <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
-            <ContentWrapper>
-                <div className="logo" onClick={() => navigate("/")}>
-                    <img src={logo} alt="" />
-                </div>
-                <ul className="menuItems">
-                    <li
-                        className="menuItem"
-                        onClick={() => navigationHandler("movie")}
-                    >
-                        Movies
-                    </li>
-                    <li
-                        className="menuItem"
-                        onClick={() => navigationHandler("tv")}
-                    >
-                        TV Shows
-                    </li>
-                    <li className="menuItem">
-                        <HiOutlineSearch onClick={openSearch} />
-                    </li>
-                </ul>
+      <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+        <ContentWrapper>
+          <div className="logo" onClick={() => navigate("/")}>
+            <img src={logo} alt="" />
+          </div>
+          <ul className="menuItems">
+            <li className="menuItem" onClick={() => navigationHandler("movie")}>
+              Movies
+            </li>
+            <li className="menuItem" onClick={() => navigationHandler("tv")}>
+              TV Shows
+            </li>
+            <li className="menuItem">
+              <HiOutlineSearch onClick={openSearch} />
+            </li>
 
-                <div className="mobileMenuItems">
-                    <HiOutlineSearch onClick={openSearch} />
-                    {mobileMenu ? (
-                        <VscChromeClose onClick={() => setMobileMenu(false)} />
-                    ) : (
-                        <SlMenu onClick={openMobileMenu} />
-                    )}
-                </div>
-            </ContentWrapper>
-            {showSearch && (
-                <div className="searchBar">
-                    <ContentWrapper>
-                        <div className="searchInput">
-                            <input
-                                type="text"
-                                placeholder="Search for a movie or tv show...."
-                                onChange={(e) => setQuery(e.target.value)}
-                                onKeyUp={searchQueryHandler}
-                            />
-                            <VscChromeClose
-                                onClick={() => setShowSearch(false)}
-                            />
-                        </div>
-                    </ContentWrapper>
-                </div>
+            <li className="white">{isAuthenticated && <p>{user.name}</p>}</li>
+
+            {
+                isAuthenticated ? (
+                    <li>
+                          <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out </button>
+                    </li>
+                ) : (
+                    <li>
+                        <button onClick={() => loginWithRedirect()}>Log In</button>;
+                    </li>
+                )
+            }
+          </ul>
+
+          <div className="mobileMenuItems">
+            <HiOutlineSearch onClick={openSearch} />
+            {mobileMenu ? (
+              <VscChromeClose onClick={() => setMobileMenu(false)} />
+            ) : (
+              <SlMenu onClick={openMobileMenu} />
             )}
-        </header>
+          </div>
+        </ContentWrapper>
+        {showSearch && (
+          <div className="searchBar">
+            <ContentWrapper>
+              <div className="searchInput">
+                <input
+                  type="text"
+                  placeholder="Search for a movie or tv show...."
+                  onChange={(e) => setQuery(e.target.value)}
+                  onKeyUp={searchQueryHandler}
+                />
+                <VscChromeClose onClick={() => setShowSearch(false)} />
+              </div>
+            </ContentWrapper>
+          </div>
+        )}
+      </header>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from "./App";
 import "./index.scss";
 
@@ -7,7 +8,17 @@ import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+
+    <Auth0Provider
+    domain="dev-z25hjkz5wzzcssfc.us.auth0.com"
+    clientId="I8uqHqz59Heq3q1ShDSQACV0TSHtBr1G"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+         
     <Provider store={store}>
         <App />
     </Provider>
+
+  </Auth0Provider>,
 );
